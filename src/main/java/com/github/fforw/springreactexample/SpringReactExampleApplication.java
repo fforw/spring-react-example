b7c54faf69780afe79172a5581367c58b323dcb3
Sven Helmberger <fforw@gmx.de>
2017-11-06 02:03:12 +0100

    Create project skeleton
    Create basic spring boot web project with spring initializer:
    
    https://start.spring.io/

---
+++ src/main/java/com/github/fforw/springreactexample/SpringReactExampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.fforw.springreactexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactExampleApplication.class, args);
+	}
+}
